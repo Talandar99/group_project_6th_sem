6b89b20b674cda9bec22d7c3ad0a9adb212bff57
klaudiamlynska <klaudiamlynskaa@gmail.com>
2025-04-09 22:28:54 +0200

    dodanie listy oraz detali produktu

---
+++ frontend/lib/widgets/product_tile.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/models/product_model.dart';
+import '../theme/app_colors.dart';
+import '../theme/app_text_styles.dart';
+import 'custom_button.dart';
+import 'custom_outlined_button.dart';
+
+class ProductTile extends StatelessWidget {
+  final ProductModel product;
+  final VoidCallback onAddCart;
+  final VoidCallback onDetails;
+
+  const ProductTile({
+    super.key,
+    required this.product,
+    required this.onAddCart,
+    required this.onDetails,
+});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: AppColors.shadow,
+            blurRadius: 15,
+            offset: Offset(0,4),
+          )
+        ],
+      ),
+        child: Column (
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(product.name, style: AppTextStyles.subheading),
+            const SizedBox(height: 5),
+            Text('${product.price.toStringAsFixed(2)} zł', style: AppTextStyles.body),
+            const SizedBox(height: 10),
+            Row(
+              children: [
+                Expanded(
+                    child: CustomButton(
+                        text: 'Do koszyka',
+                        onPressed: onAddCart,
+                    ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: CustomOutlinedButton(
+                    text: 'Szczegóły',
+                    onPressed: onDetails,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        )
+    );
+    throw UnimplementedError();
+  }
+}
